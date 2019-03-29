5855e926094b1b30a454459110b39874670fc999
YannBachelot <49059980+YannBachelot@users.noreply.github.com>
2019-03-29 09:50:18 +0100

    Modélisation frequence cardiaque
    fréquence cardiaque en fonction de cadence

---
+++ heart rate = f(power,cadence).R
@@ -0,0 +1,24 @@
+Eq1 <- function(t,x,parms)
+{
+  x[1] <- (parms[1]/(exp(-4)*x[2]**(1.6)*exp(-0.02*parms[2])))**(1/0.85)+parms[3]
+  list(x)
+}
+# parms = P, C, Hmini
+# x = H
+
+temps <- seq(0, 100000, by=60)
+cond1 = c(300, 90, 55)
+cond2 = c(300, 120, 55)
+cond3 = c(300, 60, 55)
+
+y <- Eq1(temps, parms=cond1)
+y2 <- Eq1(temps, parms = cond2)
+y3 <- Eq1(temps, parms = cond3)
+plot (90, y)
+points(120, y2)
+points(60, y3)
+y
+y2
+y3
+cadence <- seq(40, 160, by = 1)
+y <- Eq1(temps, x = cadence)
