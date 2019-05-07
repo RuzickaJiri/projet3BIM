fc09ee4bdfc7b2a6b8e284715d6270c3667bf82b
RuzickaJiri <46133604+RuzickaJiri@users.noreply.github.com>
2019-05-07 15:58:13 +0200

    Continuation equation model
    Development of the modelization ; lactate and intensity

--- lactate.R
+++ lactate.R
@@ -26,3 +26,40 @@ plot(nu,y2)
 
 L_fin = y1*y2
 plot(temps, L_fin)
+
+
+#####################################
+#Puissance equation
+Puissance <- function(x,parms)
+{
+  y <- parms[1] * x^parms[2]*exp(-parms[3]*x[1])*160^0.86
+  #*x[2]^parms[4] 
+  
+}
+
+param = c(exp(-4),1.6,0.02,0.85)
+cad = seq(30.3, 180, by = 0.3)
+heart_rate = seq(50, 200, by = 1)
+#x = data.frame(cad,heart_rate)
+
+puis = Puissance(x=cad, parms= param)
+plot(cad, puis)
+puis_const = rep(300,500)
+
+# avec la cadence (intermediaire puissance)
+y3 <- L_nu(x=puis_const, parms= parameters)
+
+L_fin_puis = y1*y3
+plot(cad, L_fin_puis,ylim = c(0,10))
+# on remarque que la cadence qui optimise la fatigue
+# en prenant la plus grande puissance possible
+# est cad = 68,7
+
+# pistes pour la prochaine fois:
+# couples puissance : cadence (moyenne de puissance)
+# puissance constante (voir au_dessus)
+
+
+
+
+
